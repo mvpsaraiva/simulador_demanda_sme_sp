e738fa603c579a387ca7abc0455e18eec1cdd907
Marcus Saraiva <marcus.saraiva@gmail.com>
2023-01-17 22:46:53 -0300

    Added administration mode.

--- R/run_app.R
+++ R/run_app.R
@@ -18,7 +18,7 @@ run_app <- function(
 ) {
   with_golem_options(
     app = shinyApp(
-      ui = shinymanager::secure_app(app_ui),
+      ui = shinymanager::secure_app(app_ui, enable_admin = TRUE, language = "pt-BR"),
       server = app_server,
       onStart = onStart,
       options = options,
